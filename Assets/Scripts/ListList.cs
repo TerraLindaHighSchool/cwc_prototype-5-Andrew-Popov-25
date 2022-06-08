aba7907ec467af80fc7300dce89ade793be3c0a5
Andrew-Popov-25 <89489268+Andrew-Popov-25@users.noreply.github.com>
2022-06-08 09:04:51 -0700

    Added comments to ListList.cs

--- Assets/Scripts/ListList.cs
+++ Assets/Scripts/ListList.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//List of GameObjects
 [System.Serializable]
 public class GameObjList
 {
     public List<GameObject> list;
 }
 
+//List of GameObjLists = List of List of GameObjects
+//For groups of any size, with however many groups are needed.
 [System.Serializable]
 public class ListList
 {
